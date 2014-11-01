8e196aa65824b1bee0e8f382379b2942ae27f6a1
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2014-11-01 09:21:27 +0300

    Initial commit

---
+++ src/main/java/ru/spb/kupchinolabs/webscraper/BootStrap.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) by Kupchino Labs
+ * created: 1/11/2014 (dd/MM/yyyy)
+ * author: Vladimir Krasilschik
+ */
+
+package ru.spb.kupchinolabs.webscraper;
+
+public class BootStrap {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/test/java/ru/spb/kupchinolabs/webscraper/TestBootStrap.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) by Kupchino Labs
+ * created: 1/11/2014 (dd/MM/yyyy)
+ * author: Vladimir Krasilschik
+ */
+
+package ru.spb.kupchinolabs.webscraper;
+
+import org.junit.Test;
+
+public class TestBootStrap {
+    @Test
+    public void hello(){
+        new BootStrap().main(null);
+    }
+}

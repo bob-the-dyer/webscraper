7dbc4dff4c6573e268ead10c76c26c300849fbe3
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2014-11-02 02:13:18 +0300

    adding sources and docs generation move  to jdk 1.8

--- src/main/java/ru/spb/kupchinolabs/webscraper/BootStrap.java
+++ src/main/java/ru/spb/kupchinolabs/webscraper/BootStrap.java
@@ -7,7 +7,52 @@
 package ru.spb.kupchinolabs.webscraper;
 
 public class BootStrap {
+
+    final static private String help =
+            "Please use the following format to run webscraper:\n" +
+                    "java –jar scraper.jar <url> [words] <commands>\n" +
+                    "where:\n" +
+                    "   <url> - web link like http://www.cnn.com of path to file like file:///Users/Vladimir/hireright/webscraper/url.list\n" +
+                    "   [words] - comma-separated words to scrap. Non mandatory for command [-c]\n" +
+                    "   <commands> - supported\n" +
+                    "       –v[erbose]\n" +
+                    "       –w[ords count]\n" +
+                    "       –c[hars on page]\n" +
+                    "       –e[xtract sentences with words]\n";
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("=====================");
+        System.out.println("==  webscraper 1.0 ==");
+        System.out.println("=====================");
+        if (args == null || args.length < 2) {
+            System.out.println("At least URL, one word or one command should be provided as arguments.\n" + help);
+            throw new IllegalArgumentException();
+        }
+        final String url = args[0];
+        if (url == null || url.length() == 0 || (!url.startsWith("http://") && !url.startsWith("file://"))) {
+            System.out.println("Url is not specified\n" + help);
+            throw new IllegalArgumentException();
+        }
+        final String arg1 = args[1];
+        if (args.length == 2) {
+            if ("-c".equalsIgnoreCase(arg1)) {
+                System.out.println("Counting the number of chars for url(s)");
+                //TODO 1
+            } else {
+                System.out.println("Words and commands must be specified\n" + help);
+                throw new IllegalArgumentException();
+            }
+        } else {
+            final String arg2 = args[2];
+            if (args.length == 3) {
+                if ("-c".equalsIgnoreCase(arg1) && "-v".equalsIgnoreCase(arg2) || "-c".equalsIgnoreCase(arg2) && "-v".equalsIgnoreCase(arg1)) {
+                    System.out.println("Counting the number of chars for url(s) in verbose mode");
+                    //TODO 2
+                } else {
+                    System.out.println("Confusing list of commands\n" + help);
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
     }
 }
--- src/test/java/ru/spb/kupchinolabs/webscraper/TestBootStrap.java
+++ src/test/java/ru/spb/kupchinolabs/webscraper/TestBootStrap.java
@@ -9,8 +9,39 @@ package ru.spb.kupchinolabs.webscraper;
 import org.junit.Test;
 
 public class TestBootStrap {
+    @Test(expected = IllegalArgumentException.class)
+    public void emptyArgs() {
+        new BootStrap().main(new String[0]);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void notEnoughArgs() {
+        new BootStrap().main(new String[]{"-c"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void noUrlInArgs() {
+        new BootStrap().main(new String[]{"Greece,default", "-c"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void noWordsAndNoCharCountCommand() {
+        new BootStrap().main(new String[]{"http://cnn.com", "-w"});
+    }
+
     @Test
-    public void hello(){
-        new BootStrap().main(null);
+    public void noWordsAndCharCountCommand() {
+        new BootStrap().main(new String[]{"http://cnn.com", "-c"});
+    }
+
+    @Test
+    public void noWordsAndCharCountCommandWithVerbose() {
+        new BootStrap().main(new String[]{"http://cnn.com", "-c", "-v"});
+        new BootStrap().main(new String[]{"http://cnn.com", "-v", "-c"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void noWordsAndCharCountCommandWithWordsCounting() {
+        new BootStrap().main(new String[]{"http://cnn.com", "-c", "-w"});
     }
 }

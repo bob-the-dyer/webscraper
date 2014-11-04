58379b6313c9f7292882d0c15bdcacab0f860399
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2014-11-04 07:39:44 +0300

    commands dispatching

--- src/main/java/ru/spb/kupchinolabs/webscraper/BootStrap.java
+++ src/main/java/ru/spb/kupchinolabs/webscraper/BootStrap.java
@@ -10,44 +10,83 @@ import org.apache.commons.cli.*;
 
 public class BootStrap {
 
+    public static final String WORDS_COUNT_OPTION = "w";
+    public static final String CHARS_COUNT_OPTION = "c";
+    public static final String SENTENCES_OPTION = "e";
+    public static final String VERBOSE_OPTION = "v";
+    public static final String FILE_OPTION = "file";
+    public static final String URL_OPTION = "url";
+    public static final String WORDS_OPTION = "words";
+    public static final String COMMAND_LINE_EXAMPLE = "java –jar webscraper-jar-with-dependencies.jar";
+
     public static void main(String[] args) {
-        System.out.println("=====================");
-        System.out.println("==  webscraper 1.0 ==");
-        System.out.println("=====================");
+        System.out.println("================================");
+        System.out.println("==  Welcome to webscraper 1.0 ==");
+        System.out.println("================================");
 
         final CommandLine cmd = cmd(args);
-        if (cmd == null) System.exit(-1);
 
-        //TODO next check options and start logic
+        if (cmd != null) {
+            dispatchProcessing(cmd);
+        }
+    }
 
+    protected static boolean dispatchProcessing(CommandLine cmd) {
+        if (cmd.hasOption(URL_OPTION)) {
+            boolean goodOptions = false;
+            if (cmd.hasOption(WORDS_OPTION) && cmd.hasOption(WORDS_COUNT_OPTION)) {
+                //TODO logic for url and words counting
+                goodOptions = true;
+            }
+            if (cmd.hasOption(CHARS_COUNT_OPTION)) {
+                //TODO logic for url and chars counting
+                goodOptions = true;
+            }
+            if (!goodOptions) {
+                System.out.println("Neither " + WORDS_COUNT_OPTION + " nor " + CHARS_COUNT_OPTION + " commands were specified correctly.");
+                new HelpFormatter().printHelp(COMMAND_LINE_EXAMPLE, constructOptions(), true);
+            }
+            return goodOptions;
+        } else if (cmd.hasOption(FILE_OPTION)) {
+            System.out.println(FILE_OPTION + " option is not supported yet");
+            //TODO logic for file
+            return true;
+        } else {
+            System.out.println("Neither " + URL_OPTION + " nor " + FILE_OPTION + " commands were specified.");
+            new HelpFormatter().printHelp(COMMAND_LINE_EXAMPLE, constructOptions(), true);
+            return false;
+        }
     }
 
     protected static CommandLine cmd(String[] args) {
-        Options options = new Options();
-        options.addOption(new Option("w", false, "count number of provided word(s) occurrences on webpage(s)"));
-        options.addOption(new Option("c", false, "count number of characters of each web page"));
-        options.addOption(new Option("e", false, "extract sentences which contain given words"));
-        options.addOption(new Option("v", false, "output verbosity flag, if on then the output should contains information about time spent on data scraping and data processing"));
-        options.addOption(OptionBuilder.withArgName("path")
-                .hasArg()
-                .withDescription("path to plain text file containing a list of URLs")
-                .create("file"));
-        options.addOption(OptionBuilder.withArgName("url")
-                .hasArg()
-                .withDescription("web resources URL")
-                .create("url"));
-        options.addOption(OptionBuilder.withArgName("words")
-                .hasArg()
-                .withDescription("word or list of words with ',' delimiter")
-                .create("words"));
-
+        Options options = constructOptions();
         try {
             return new BasicParser().parse(options, args);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("java –jar webscraper-jar-with-dependencies.jar", options, true);
+            new HelpFormatter().printHelp("java –jar webscraper-jar-with-dependencies.jar", options, true);
             return null;
         }
+    }
+
+    private static Options constructOptions() {
+        Options options = new Options();
+        options.addOption(new Option(WORDS_COUNT_OPTION, false, "count number of provided word(s) occurrences on webpage(s)"));
+        options.addOption(new Option(CHARS_COUNT_OPTION, false, "count number of characters of each web page"));
+        options.addOption(new Option(SENTENCES_OPTION, false, "extract sentences which contain given words"));
+        options.addOption(new Option(VERBOSE_OPTION, false, "output verbosity flag, if on then the output should contains information about time spent on data scraping and data processing"));
+        options.addOption(OptionBuilder.withArgName("path")
+                .hasArg()
+                .withDescription("path to plain text file containing a list of URLs")
+                .create(FILE_OPTION));
+        options.addOption(OptionBuilder.withArgName("url")
+                .hasArg()
+                .withDescription("web resources URL")
+                .create(URL_OPTION));
+        options.addOption(OptionBuilder.withArgName("words")
+                .hasArg()
+                .withDescription("word or list of words with ',' delimiter")
+                .create(WORDS_OPTION));
+        return options;
     }
 }
--- src/test/java/ru/spb/kupchinolabs/webscraper/TestBootStrap.java
+++ src/test/java/ru/spb/kupchinolabs/webscraper/TestBootStrap.java
@@ -9,22 +9,41 @@ package ru.spb.kupchinolabs.webscraper;
 import org.apache.commons.cli.CommandLine;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class TestBootStrap {
 
-    @Test()
+    @Test
     public void testGoodArgs() throws Exception {
         final BootStrap bootStrap = new BootStrap();
         assertNotNull(bootStrap.cmd(new String[]{"-url", "http://www.cnn.com", "-words", "Greece,default", "–v", "–w", "–c", "–e"}));
         assertNotNull(bootStrap.cmd(new String[]{"-file", "/Users/scraper-jar-with-dependencies.jar", "-words", "Greece", "–w", "–c"}));
     }
 
-    @Test()
+    @Test
     public void testUnknownArgs() throws Exception {
         final BootStrap bootStrap = new BootStrap();
         final CommandLine cmd = bootStrap.cmd(new String[]{"-url", "http://www.cnn.com", "-swords", "Greece,default", "–g", "–h", "–m", "–l"});
         assertNull(cmd);
+    }
+
+    @Test
+    public void testMandatoryCommandsUrlOrFile() {
+        final BootStrap bootStrap = new BootStrap();
+        final CommandLine cmd = bootStrap.cmd(new String[]{"-words", "Greece,default", "–v", "–w", "–c", "–e"});
+        assertFalse(bootStrap.dispatchProcessing(cmd));
+    }
+
+    @Test
+    public void testMandatoryCommandsWordsAndCounts() {
+        final BootStrap bootStrap = new BootStrap();
+        CommandLine cmd = bootStrap.cmd(new String[]{"-url", "http://www.cnn.com", "-words", "Greece,default", "-v"});
+        assertFalse(bootStrap.dispatchProcessing(cmd));
+        cmd = bootStrap.cmd(new String[]{"-url", "http://www.cnn.com", "-w"});
+        assertFalse(bootStrap.dispatchProcessing(cmd));
+        cmd = bootStrap.cmd(new String[]{"-url", "http://www.cnn.com", "-words", "Greece,default", "-c"});
+        assertTrue(bootStrap.dispatchProcessing(cmd));
+        cmd = bootStrap.cmd(new String[]{"-url", "http://www.cnn.com", "-c"});
+        assertTrue(bootStrap.dispatchProcessing(cmd));
     }
 }
